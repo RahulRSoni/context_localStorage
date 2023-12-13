ac4d361c75f03d5caf7e3ed89530fe0bd0ea8eeb
RahulRSoni <114871652+RahulRSoni@users.noreply.github.com>
2023-12-13 15:44:33 +0530

    bug fix

--- context_localStorage/src/App.jsx
+++ context_localStorage/src/App.jsx
@@ -24,6 +24,17 @@ function App() {
 		});
 	};
 
+	useEffect(() => {
+		const todos = JSON.parse(localStorage.getItem('todos'));
+		if (todos && todos.length < 0) {
+			setTodos(todos);
+		}
+	}, []);
+
+	useEffect(() => {
+		localStorage.setItem('todos', JSON.stringify(todos));
+	}, [todos]);
+
 	const toggleComplete = (id) => {
 		setTodos((prev) => {
 			prev.map((prevTodo) => {
@@ -33,15 +44,6 @@ function App() {
 			});
 		});
 	};
-
-	useEffect(() => {
-		const todos = JSON.parse(localStorage.getItem('todos'));
-		if (todos && todos.length > 0) setTodos(todos);
-	}, []);
-
-	useEffect(() => {
-		localStorage.setItem('todos', JSON.stringify('todos'));
-	}, [todos]);
 
 	return (
 		<TodoProvider
